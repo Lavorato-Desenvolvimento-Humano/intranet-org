75b32f4132afab2b95e2e1ca636ec6caa6ddb5fb
Vincius <viniciusgomes.33mes@gmail.com>
2025-02-25 20:56:42 -0300

    Primeira organização do repositório

---
+++ apps/backend/src/main/java/com/intranet/backend/controllers/HelloController.java
@@ -0,0 +1,17 @@
+package com.intranet.backend.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Olá, o backend está funcionando!";
+    }
+}
