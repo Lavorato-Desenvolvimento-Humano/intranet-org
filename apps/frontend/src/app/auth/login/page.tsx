6151a2a514dbbcc8dc37f9a1eb3fa3f43d0452b3
Vincius <viniciusgomes.33mes@gmail.com>
2025-03-03 12:30:05 -0300

    fix: ajuste de path do tsconfig.json

--- apps/frontend/src/app/auth/login/page.tsx
+++ apps/frontend/src/app/auth/login/page.tsx
@@ -3,7 +3,7 @@
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import Input from "@/components/Input";
+import Input from "@components/Input";
 
 const loginSchema = z.object({
   email: z.string().email("E-mail inválido"),
