838279af4d23241ba23d4611208f106ae7036a4a
ViniciusG03 <viniciusgomes.33mes@gmail.com>
2025-03-04 12:28:29 -0300

    test: teste da pipeline e dockerfile

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <h1>Olá, Intranet-org</h1>
-      <p>Este é uma simples página para testar o next.js</p>
+      <p>Este é uma simples página para testar o next.js!</p>
     </div>
-  )
+  );
 }
