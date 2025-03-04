5ef8a9d0d01ddca5090342dcdc3fe7c7e591b066
ViniciusG03 <viniciusgomes.33mes@gmail.com>
2025-03-04 18:11:39 -0300

    test: conexão com o backend

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,10 +1,23 @@
-import React from "react";
+"use client";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/hello")
+      .then((res) => res.text())
+      .then((data) => setMessage(data));
+  }, []);
+
   return (
-    <div>
-      <h1>Olá, Intranet-org</h1>
-      <p>Este é uma simples página para testar o next.js</p>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Frontend conectado ao Backend!
+      </h1>
+      <p className="mt-4 text-lg text-gray-700">
+        Resposta do Backend: {message}
+      </p>
     </div>
   );
 }
