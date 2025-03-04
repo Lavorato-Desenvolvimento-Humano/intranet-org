059bae2e3e23fb0901f30fb4ab5a354b2653039a
ViniciusG03 <viniciusgomes.33mes@gmail.com>
2025-03-04 20:34:48 -0300

    test: ajuste no cors

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   const [message, setMessage] = useState("");
 
   useEffect(() => {
-    fetch("http://backend:8080/api/hello")
+    fetch("https://dev.lavorato.app.br:8080/api/hello")
       .then((res) => res.text())
       .then((data) => setMessage(data));
   }, []);
