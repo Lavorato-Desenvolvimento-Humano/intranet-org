4ff1bde1d5b0c0254824645deedf8400b476d448
ViniciusG03 <viniciusgomes.33mes@gmail.com>
2025-03-04 12:02:33 -0300

    test: correcao do nextjs config

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -4,11 +4,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  experimental: {
-    optimizeCss: {
-      tailwindcss: true
-    }
-  }
 };
 
 export default nextConfig;
