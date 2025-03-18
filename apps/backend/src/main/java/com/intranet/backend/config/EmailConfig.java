d0d85dc45d62b18cffeefb9839aecc8ba120d92b
Vincius <viniciusgomes.33mes@gmail.com>
2025-03-18 13:06:39 -0300

    update: Emailconfig para usar as informações do application properties

--- apps/backend/src/main/java/com/intranet/backend/config/EmailConfig.java
+++ apps/backend/src/main/java/com/intranet/backend/config/EmailConfig.java
@@ -1,5 +1,6 @@
 package com.intranet.backend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -18,21 +19,34 @@ import java.util.Properties;
 @Configuration
 public class EmailConfig {
 
+    @Value("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.port}")
+    private int port;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
     // Configuração do JavaMailSender
     @Bean
     public JavaMailSender mailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
 
-        // Estas propriedades serão substituídas pelas que estão no application.properties
-        mailSender.setHost("email-ssl.com.br");
-        mailSender.setPort(587);
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.ssl.trust", "email-ssl.com.br");
-        props.put("mail.debug", "false");
+        props.put("mail.smtp.ssl.trust", host);
+        props.put("mail.debug", "true"); // Para depuração
 
         return mailSender;
     }
