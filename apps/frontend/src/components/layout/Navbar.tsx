dd3a5594c6e10a9f8e0b5d7ae8cac7d61c479109
Vincius <viniciusgomes.33mes@gmail.com>
2025-04-05 10:17:10 -0300

    update: icon

--- apps/frontend/src/components/layout/Navbar.tsx
+++ apps/frontend/src/components/layout/Navbar.tsx
@@ -14,6 +14,7 @@ import {
   Users,
   Menu,
   ChevronDown,
+  Book,
 } from "lucide-react";
 import { useRouter } from "next/navigation";
 
@@ -121,7 +122,7 @@ export default function Navbar() {
                     href="/demandas"
                     className="flex items-center px-4 py-2 hover:bg-gray-100"
                     onClick={handleNavigate("/demandas")}>
-                    <Users className="mr-2" size={16} />
+                    <Book className="mr-2" size={16} />
                     <span>Demandas</span>
                   </Link>
 
