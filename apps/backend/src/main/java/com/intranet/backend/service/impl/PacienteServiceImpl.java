c0e9a0825aed599a374e3bd6e3834541b5809a03
Vincius <viniciusgomes.33mes@gmail.com>
2025-06-09 10:14:56 -0300

    hotfix dto

--- apps/backend/src/main/java/com/intranet/backend/service/impl/PacienteServiceImpl.java
+++ apps/backend/src/main/java/com/intranet/backend/service/impl/PacienteServiceImpl.java
@@ -236,6 +236,7 @@ public class PacienteServiceImpl implements PacienteService {
         return new GuiaSummaryDto(
                 guia.getId(),
                 guia.getNumeroGuia(),
+                guia.getStatus(),
                 guia.getPaciente().getNome(),
                 guia.getEspecialidades(),
                 guia.getQuantidadeAutorizada(),
