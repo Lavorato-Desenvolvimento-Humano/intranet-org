c495123db1bdb6e5084227d3a6536407121b0550
Vinicius Oliveira <viniciusgomes.33mes@gmail.com>
2025-09-04 16:10:23 -0300

    update: correção de build de dependência

--- drive-microservices/services/drive-file-service/src/main/java/com/intranet/drive/file/config/DriveFileServiceConfig.java
+++ drive-microservices/services/drive-file-service/src/main/java/com/intranet/drive/file/config/DriveFileServiceConfig.java
@@ -1,23 +1,8 @@
 package com.intranet.drive.file.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import com.intranet.drive.common.security.SecurityConfig;
-import com.intranet.drive.common.security.JwtAuthenticationFilter;
-import com.intranet.drive.common.service.CoreIntegrationService;
-import com.intranet.drive.common.security.JwtTokenUtil;
-import com.intranet.drive.common.config.JpaAuditConfig;
-import com.intranet.drive.common.config.RetryConfig;
 
 @Configuration
-@Import({
-        SecurityConfig.class,
-        JwtAuthenticationFilter.class,
-        CoreIntegrationService.class,
-        JwtTokenUtil.class,
-        JpaAuditConfig.class,
-        RetryConfig.class,
-})
 public class DriveFileServiceConfig {
     //Configuração específica para fazer carregar as classes do drive-common
 }
